cd594872504839ae913d0071eb278324c2a69339
Алексей Семионов <alehaaaaa@mail.ru>
2025-05-01 17:18:25 +0300

    Panel & Fill DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<conio.h>
 #include<Windows.h>
-using namespace std;
+#include<thread>
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define Escape	27
 #define Enter	13
@@ -121,6 +124,10 @@ class Car
 	int speed;
 	const int MAX_SPEED;
 	bool driver_inside;
+	struct
+	{
+		std::thread panel_thread;
+	}threads_container;	//Ёта структура не имеет имени и реализует только один жкземпл€р
 public:
 	Car(double consumption, int capacity, int max_speed = 250) :
 		MAX_SPEED
@@ -143,11 +150,15 @@ public:
 	void get_in()
 	{
 		driver_inside = true;
-		panel();
+		threads_container.panel_thread = std::thread(&Car::panel, this);
+		//panel();
 	}
 	void get_out()
 	{
 		driver_inside = false;
+		if (threads_container.panel_thread.joinable())threads_container.panel_thread.join();
+		system("CLS");
+		cout << "You are out of the Car" << endl;
 	}
 	void control()
 	{
@@ -160,8 +171,16 @@ public:
 			case Enter:
 				driver_inside ? get_out() : get_in();
 				break;
+			case 'F':case'f':
+				double fuel;
+				cout << "¬ведите обьем топлива: "; cin >> fuel;
+				tank.fill(fuel);
+				break;
+			case Escape:
+				get_out();
 			}
 		} while (key != Escape);
+		//Concutent execution
 	}
 	void panel()
 	{
@@ -209,3 +228,11 @@ void main()
 	//bmv.info();
 	bmv.control();
 }
+
+/*
+-------------------------------------------------------------------
+Thread - этопоследовательность комманд цп
+Ћюба€ программа выполн€етс€ миниум в один поток
+
+-----------------------------------------------------------------
+*/
